fd411033415b93fdab9cba5b47e9ef099077b44e
CaianSantana <caian23@outlook.com>
2024-01-11 00:28:40 -0300

    vender produto

--- Produtos.php
+++ Produtos.php
@@ -14,12 +14,11 @@ class Produto implements validador{
     public $nome;
     public $preco;
     public $quantidade;
-    public $cadastrado = false;
+    public $cadastrado = false; //booleano usado para verificar se o produto já foi preenchido com informações válidas
 
-    public function validar($vetor) {
+    public function validar($vetor) { // Verifica se todos os atributos esperados existem no vetor e se estão ou não vazios
         // Atributos esperados para a classe Produto
         $atributosEsperados = array('nome', 'preco', 'quantidade');
-        // Verifica se todos os atributos esperados existem no vetor e se estão ou não vazios
         foreach ($atributosEsperados as $atributo) {
             if (empty($vetor[$atributo])) {
                 return false;
@@ -38,20 +37,28 @@ class Produto implements validador{
         $this->nome = $data['nome'];
         $this->preco = $data['preco'];
         $this->quantidade = $data['quantidade'];
-        $this->cadastrado = true;
+        $this->cadastrado = true; //coloca o valor de cadastrado como verdadeiro, assim é possível identificar que o produto já foi cadastrado corretamente
     
         echo "<br>Produto $this->nome registrado!";
     }
 
-    public function getProduto(){
+    public function getProduto(){ //verifica se o produto não está cadastrado e, se não estiver, exibe log informando isso. Caso esteja cadastrado, exibe suas informações
         if(!$this->cadastrado){
-            echo "<br>Produto não cadastrado!";
+            echo "<br>Erro!";
+            error_log("Produto não cadastrado.", 0);
             return;
         }
         echo "<br>Nome: $this->nome, Preço: $this->preco, Quantidade: $this->quantidade";
     }
     
-    
+    public function vender($quantidadeVendida){
+        if(!is_int($quantidadeVendida) || $quantidadeVendida<0 || $quantidadeVendida> $this->quantidade){
+            echo "<br>Erro!";
+            error_log("Quantidade inválida.", 0);
+            return;
+        }
+        $this->quantidade -= $quantidadeVendida;
+    }
 }
 
 // Exemplo de uso da classe Produto
@@ -67,6 +74,8 @@ $dados = array(
 
 // Chamando o método setProduto para cadastrar os dados
 $produto1->setProduto($dados);
+$produto1->getProduto();
+$produto1->vender(10);
 $produto1->getProduto();
 $produto2->getProduto();
 
---
+++ Vendas.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Após a conclusão da classe Produto, deve-se criar a classe Venda, que herdará os
+atributos da classe produto, possuindo em seus próprios atributos a quantidade e
+desconto.
+Dentro da classe `Venda`, devem ser implementados os seguintes métodos:
+- getVenda;
+- setVenda.
+O método setVenda é responsável pelo registro da venda. Inicialmente, ele deve
+verificar se o produto está cadastrado. Em caso afirmativo, deve validar o estoque do
+produto e, em seguida, permitir a venda do produto. Após a conclusão da venda, é
+necessário utilizar o método getVenda para exibir a última venda registrada e informar o
+estoque atual do produto. 
+*/
+
+class Vendas{
+    
+}
